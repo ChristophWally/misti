7aa6bf02368879fa5af4f12deb15904bf505564e
ChristophWally <104388367+ChristophWally@users.noreply.github.com>
2025-07-05 11:51:11 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -284,7 +284,7 @@ export default function RootLayout({ children }) {
                           onclick="playAudio('\${word.id}', '\${word.italian}')"
                           title="Play pronunciation"
                         >
-                          <svg width="12" height="12" viewBox="0 0 24 24" fill="currentColor">
+                          <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
                             <path d="M8 5v14l11-7z"/>
                           </svg>
                         </button>
@@ -408,7 +408,7 @@ export default function RootLayout({ children }) {
                 
                 // Show loading state with pause icon
                 audioBtn.innerHTML = \`
-                  <svg width="12" height="12" viewBox="0 0 24 24" fill="currentColor">
+                  <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
                     <path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/>
                   </svg>
                 \`;
@@ -443,7 +443,7 @@ export default function RootLayout({ children }) {
                 } else {
                   // No TTS available - show error icon briefly
                   audioBtn.innerHTML = \`
-                    <svg width="12" height="12" viewBox="0 0 24 24" fill="currentColor">
+                    <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
                       <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
                     </svg>
                   \`;
